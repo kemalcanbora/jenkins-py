7f705f5af8102960737c3b0f62f52b211c8a9d51
kemalcan <kemalcanbora@gmail.com>
2022-02-03 23:42:51 +0300

    added fargate terraform

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "aws_account_id" {
+  description = "AWS account ID"
+  default     = ""
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given AWS region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "123740642471.dkr.ecr.us-east-1.amazonaws.com/container-app"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 8080
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 2
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = 256
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = 512
+}
